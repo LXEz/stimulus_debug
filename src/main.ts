b0ea7cda0a957e83c7a8cad4e39c7aaafc238a22
lixiantao <290790956@qq.com>
2024-04-08 18:11:15 +0800

    doc: 跟新文档

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { Application } from "./../stimulus/core/application";
 
 import { Demo } from "./controller/demo";
-
+export * from "./../stimulus/core/application";
 const application = Application.start();
 
 console.log(2222);
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,30 @@
 import { defineConfig } from "vite";
 import { resolve, dirname } from "node:path";
+import terser from "@rollup/plugin-terser";
 const STIMULUS_PATH = resolve(__dirname, "./stimulus");
 export default defineConfig({
   // ...
   build: {
+    // lib: {
+    //   entry: resolve(STIMULUS_PATH, "./index.ts"),
+    //   name: "mylib",
+    // },
     target: "esnext",
     sourcemap: true,
-    // minify: false,
+    // minify: "terser",
     rollupOptions: {
+      plugins: [
+        terser({
+          mangle: { keep_fnames: true, keep_classnames: true },
+        }),
+      ],
+
       output: {
+        minifyInternalExports: false,
         format: "esm",
         entryFileNames: "main.js",
         chunkFileNames: "stimulus/[name]",
+
         manualChunks: (id) => {
           const dir = resolve(dirname(id));
           if (dir.includes(STIMULUS_PATH)) {
