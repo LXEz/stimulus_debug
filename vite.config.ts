8b739dc0ab03b173feae59208beeb99cc3b4f4a5
lixiantao <290790956@qq.com>
2024-04-08 10:10:48 +0800

    feat: 修改打包选项

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from "vite";
-
+import { resolve, dirname } from "node:path";
+const STIMULUS_PATH = resolve(__dirname, "./stimulus");
 export default defineConfig({
   // ...
   build: {
+    target: "esnext",
     sourcemap: true,
+    // minify: false,
     rollupOptions: {
       output: {
-        entryFileNames: "stimulus.js",
+        format: "esm",
+        entryFileNames: "main.js",
+        chunkFileNames: "stimulus/[name]",
+        manualChunks: (id) => {
+          const dir = resolve(dirname(id));
+          if (dir.includes(STIMULUS_PATH)) {
+            return "stimulus.js";
+          }
+
+          return "main.js";
+        },
       },
     },
   },
