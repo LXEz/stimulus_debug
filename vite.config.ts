e7a6764aabf05a17462c2b47ae23f44adc34555a
lixiantao <290790956@qq.com>
2024-04-11 10:36:49 +0800

    feat: 配置打包文件

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,38 @@ import { resolve, dirname } from "node:path";
 import terser from "@rollup/plugin-terser";
 const STIMULUS_PATH = resolve(__dirname, "./stimulus");
 const entryFile = resolve(__dirname, "./stimulus/index.ts");
+const isBuildBundle = process.env.bundle;
+const output = !isBuildBundle
+  ? {
+      minifyInternalExports: false,
+      format: "esm",
+      entryFileNames: "main.js",
+      chunkFileNames: "stimulus/[name]",
+
+      manualChunks: (id) => {
+        const dir = resolve(dirname(id));
+        if (dir.includes(STIMULUS_PATH)) {
+          return "stimulus.js";
+        }
+
+        return "main.js";
+      },
+    }
+  : undefined;
+const lib = isBuildBundle
+  ? {
+      entry: entryFile,
+      name: "stimulus",
+      fileName: "stimulus",
+    }
+  : undefined;
 export default defineConfig({
   // ...
   build: {
-    lib: {
-      entry: entryFile,
-      name: "mylib",
-    },
+    lib,
     target: "esnext",
     sourcemap: true,
-    // minify: "terser",
+    minify: "terser",
     rollupOptions: {
       plugins: [
         terser({
@@ -20,21 +42,7 @@ export default defineConfig({
         }),
       ],
 
-      output: {
-        minifyInternalExports: false,
-        format: "esm",
-        entryFileNames: "main.js",
-        chunkFileNames: "stimulus/[name]",
-
-        manualChunks: (id) => {
-          const dir = resolve(dirname(id));
-          if (dir.includes(STIMULUS_PATH)) {
-            return "stimulus.js";
-          }
-
-          return "main.js";
-        },
-      },
+      output: output as any,
     },
   },
 });
